30475e6d3e81875cd693f7783b9d5f33ad571727
Nizijolas <nicolasdijoud@yahoo.com>
2025-12-16 14:03:22 +0100

    mise en place structure

---
+++ js/Simulation.js
@@ -0,0 +1,15 @@
+import { Drone } from "./drone.js";
+
+export class Simulation{
+
+    #propagation; //booléen
+    #drones; // tableau de drones
+    #mapGlobal;
+
+    constructor(propagation){
+        this.#propagation  = propagation;
+        //init drônes
+        //init map
+    }
+
+}
--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
+import { Simulation } from "./Simulation.js ";
+
 const map = document.getElementById("map"); //map dans le sens carte et pas structure de données petit malin ;)
-const tailleMap = 50;
+const tailleMap = 40;
 
 //Charge le svg de base
 function setMap() {
@@ -16,12 +18,30 @@ function setMap() {
             rect.setAttribute("x", x * oneTileLength);
             rect.setAttribute("y", y * oneTileLength);
             rect.setAttribute("fill", "black");
-
             //on set les id de chaque case sur ce canvas "x:y" pour gérér les changements de classe via l'id de la case après.
-            rect.setAttribute("id", `${x}:${y}`);
+            rect.setAttribute("id", `${x}:${y}`)
             map.appendChild(rect);
         }
-    }
-}
+    } // Le cases sont toutes à fill black de base et au fur et à mesure de leur découverte il faudra les passer à vert en leur passant la classe arbre
+    const base = document.getElementById("0:0") //on met la base à 0, 0;
+    base.classList.add("base");
+} 
 
 setMap();
+
+
+// Lancer la simulation en récupérant les inputs
+const lancerSimulation = document.getElementById("lancerSimulation"); // <-- le boutton
+const vitesseInput = document.getElementById("vitesse");
+const propagationInput = document.getElementById("propagation");
+
+lancerSimulation.addEventListener("click", () => {
+    console.log(vitesseInput.value); // à normaliser du coup et determinera la vitesse de l'interval
+    console.log(propagationInput.checked); // true si coché
+
+    //a voir si besoin de plus d'input, nombre d'anomalies ? 
+
+    // on passe les values des inputs à un constructeur de "Simulation", on passe le boutton lancerSimulation à displayNone
+    let simulation = new Simulation(propagationInput.checked); // par exemple
+
+})
